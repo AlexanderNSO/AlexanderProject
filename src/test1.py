c011ab1a29e9dff5690e4ea12300e83bc31f4d4c
Protezist243 <protezist243@gmail.com>
2018-12-10 00:00:17 +0700

    Исправленный

--- src/test1.py
+++ src/test1.py
@@ -2,7 +2,8 @@ import time
 
 from selenium import webdriver
 
-driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+driver = webdriver.Chrome(executable_path=
+                          r"C:\Program Files\JetBrains\PyCharm Community Edition 2018.3\bin\chromedriver.exe")
 driver.implicitly_wait(10)
 
 driver.get("https://accounts.google.com")
@@ -10,48 +11,31 @@ driver.get("https://accounts.google.com")
 create_account_button = driver.find_element_by_xpath("//*[text()='Создать аккаунт']")
 create_account_button.click()
 
+email_set = ["ddswwww@a.agfd", "aewqew@-a.a", "eqwea@a@a.a" "dasdfha@a"]
+user_dictionary = {"firstName": "Alex", "lastName": "Khudyaev", "Passwd": "QWErty123456"}
+
 first_name_field = driver.find_element_by_id("firstName")
-first_name_field.send_keys("Alex")
-
 last_name_field = driver.find_element_by_id("lastName")
-last_name_field.send_keys("Kardash")
-
 password_field = driver.find_element_by_name("Passwd")
-password_field.send_keys("Abc123456!")
-
 confirm_password_field = driver.find_element_by_name("ConfirmPasswd")
-confirm_password_field.send_keys("Abc123456!")
-
 username_field = driver.find_element_by_id("username")
-username_field.clear()
-username_field.send_keys("@a.a")
 next_button = driver.find_element_by_id("accountDetailsNext")
-next_button.click()
-assert "Имя пользователя может включать латинские буквы (a-z), цифры (0-9) и точку (.)." not in driver.page_source
-time.sleep(1)
+validation_error = "Имя пользователя может включать латинские буквы (a-z), цифры (0-9) и точку (.)."
 
-username_field = driver.find_element_by_id("username")
-username_field.clear()
-username_field.send_keys("a@-a.a")
-next_button = driver.find_element_by_id("accountDetailsNext")
-next_button.click()
-assert "Имя пользователя может включать латинские буквы (a-z), цифры (0-9) и точку (.)." not in driver.page_source
-time.sleep(1)
+def validate_username_field(email):
+    username_field.clear()
+    username_field.send_keys(email)
+    next_button.click()
+    assert validation_error not in driver.page_source
+    time.sleep(1)
 
-username_field = driver.find_element_by_id("username")
-username_field.clear()
-username_field.send_keys("a@a@a.a")
-next_button = driver.find_element_by_id("accountDetailsNext")
-next_button.click()
-assert "Имя пользователя может включать латинские буквы (a-z), цифры (0-9) и точку (.)." not in driver.page_source
-time.sleep(1)
+first_name_field.send_keys(user_dictionary["firstName"])
+last_name_field.send_keys(user_dictionary["lastName"])
+password_field.send_keys(user_dictionary["Passwd"])
+confirm_password_field.send_keys(user_dictionary["Passwd"])
 
-username_field = driver.find_element_by_id("username")
-username_field.clear()
-username_field.send_keys("a@a")
-next_button = driver.find_element_by_id("accountDetailsNext")
-next_button.click()
-assert "Имя пользователя может включать латинские буквы (a-z), цифры (0-9) и точку (.)." not in driver.page_source
-time.sleep(1)
+for i in email_set:
+    validate_username_field(i)
+
 
 driver.quit()
